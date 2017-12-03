24bd7a48898cb6a3fa6924027a501b9b855ec91d
lingyun <342637442@qq.com>
2017-12-04 05:23:08 +0800

    topic controller

--- j2ee-web/src/main/java/com/xmu/j2ee/service/impl/TopicServiceImpl.java
+++ j2ee-web/src/main/java/com/xmu/j2ee/service/impl/TopicServiceImpl.java
@@ -1,5 +1,16 @@
 package com.xmu.j2ee.service.impl;
 
-public class TopicServiceImpl {
+import org.springframework.stereotype.Service;
+
+import com.xmu.j2ee.service.TopicService;
+
+@Service("TopicService")
+public class TopicServiceImpl implements TopicService {
+
+	@Override
+	public TopicService getTopicById(Integer topicId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
